5a2bb173844964d2c399505b64f672cb166bca24
sly617620-source <sly617620@gmail.com>
2026-09-18 00:10:33 +0300

    Add files via upload

---
+++ android/settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.reader(Charsets.UTF_8).use { reader ->
+                properties.load(reader)
+            }
+        }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+    }
+
+    check(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+}
+
+include(":app")
